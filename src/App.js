454011a505c8a61d95aa92843cd202f8d1264efe
matt <matt@matts-MacBook-Pro.local>
2019-09-02 17:48:59 -0500

    working navbar extrcted to component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
 import React from 'react';
-import { Navbar, NavItem, NavDropdown, MenuItem, Nav } from 'react-bootstrap';
-
+import TeachToLiveNavbar from "./Components/TeachToLiveNavbar";
 import './App.css';
 
-function App() {
-    return (
-        <div className="App">
-            <div>
-                <Navbar>
-                    <Navbar.Header>
-                        <Navbar.Brand>
-                            <a href="#home">React-Bootstrap</a>
-                        </Navbar.Brand>
-                    </Navbar.Header>
-                    <Nav>
-                        <NavItem eventKey={1} href="#">
-                            Link
-                        </NavItem>
-                        <NavItem eventKey={2} href="#">
-                            Link
-                        </NavItem>
-                        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                            <MenuItem eventKey={3.1}>Action</MenuItem>
-                            <MenuItem eventKey={3.2}>Another action</MenuItem>
-                            <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                            <MenuItem divider />
-                            <MenuItem eventKey={3.4}>Separated link</MenuItem>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar>
-            </div>
-        </div>
-    );
-}
+class App extends React.Component {
 
+    render() {
+        return (
+            <div>
+                <TeachToLiveNavbar/>
+            </div>
+        );
+
+    }
+}
 export default App;
