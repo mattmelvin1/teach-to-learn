6dd7de6f01e2b7dbd73ab91cf4958b0d147c7a4d
matt <matt@matts-MacBook-Pro.local>
2019-09-02 17:29:46 -0500

    working navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Navbar, NavItem, NavDropdown, MenuItem, Nav } from 'react-bootstrap';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>
+                <Navbar>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="#home">React-Bootstrap</a>
+                        </Navbar.Brand>
+                    </Navbar.Header>
+                    <Nav>
+                        <NavItem eventKey={1} href="#">
+                            Link
+                        </NavItem>
+                        <NavItem eventKey={2} href="#">
+                            Link
+                        </NavItem>
+                        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                            <MenuItem eventKey={3.1}>Action</MenuItem>
+                            <MenuItem eventKey={3.2}>Another action</MenuItem>
+                            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                            <MenuItem divider />
+                            <MenuItem eventKey={3.4}>Separated link</MenuItem>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar>
+            </div>
+        </div>
+    );
 }
 
 export default App;
