c9dc1a70dfb20a34811247db12012098e702a3ba
Matt Melvin <mattmelvin1@gmail.com>
2019-09-02 19:39:35 -0500

    adding buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,78 @@
 import React from 'react';
+import {Button} from "react-bootstrap";
 import TeachToLiveNavbar from "./Components/TeachToLiveNavbar";
+
+
 import './App.css';
+import "./Styles/HomepageButton.css";
+
+
 
 class App extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        // Store.configureStore()
+        this.state = {
+            showStudentButton: true,
+            showTeacherButton: true
+        }
+
+    }
+
+    _onClickStudentButton() {
+        this
+            .props
+            .history
+            .push('/studentlogin')
+        this.setState({
+            showTeacherButton: !this.state.showTeacherButton
+        })
+        this.setState({
+            showStudentButton: !this.state.showStudentButton
+        })
+    }
+
+    _onClickTeacherButton() {
+        this
+            .props
+            .history
+            .push('/login')
+        this.setState({
+            showTeacherButton: !this.state.showTeacherButton
+        })
+        this.setState({
+            showStudentButton: !this.state.showStudentButton
+        })
+    }
 
     render() {
         return (
             <div>
                 <TeachToLiveNavbar/>
+
+                <div className="HomepageButton">
+                    {this.state.showStudentButton && <Button
+                        className="ButtonSizing"
+                        variant="primary"
+                        size="lg"
+                        block
+                        onClick={this
+                        ._onClickStudentButton
+                        .bind(this)}
+                        type="button">I am a student</Button>}
+                </div>
+
+                <div className="HomepageButton">
+                    {this.state.showTeacherButton && <Button
+                        className="ButtonSizing"
+                        variant="primary"
+                        size="lg"
+                        block
+                        onClick={this
+                        ._onClickTeacherButton
+                        .bind(this)}
+                        type="button">I am an instructor</Button>}
+                </div>
             </div>
         );
 
