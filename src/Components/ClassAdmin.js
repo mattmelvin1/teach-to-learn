ff98c0a0b141492b08a9e29c5414aafbda090ef6
Matt Melvin <mattmelvin1@gmail.com>
2019-09-18 05:16:51 -0500

    updating buildpack

---
+++ src/Components/ClassAdmin.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import "../Styles/Button.css";
+
+
+export default class Class extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    _handleStudentSubmit = event => {
+        event.preventDefault();
+        this
+            .props
+            .history
+            .push('/students')
+
+    }
+    _handleClassSubmit = event => {
+        event.preventDefault();
+        this
+            .props
+            .history
+            .push('/quizzes')
+
+    }
+    render() {
+        return (
+
+            <div >
+                <div className="ReusableButton">
+                    <Button
+                        variant="primary"
+                        size="lg"
+                        block
+                        onClick={this._handleStudentSubmit}>
+                        Manage students
+                    </Button>
+                </div>
+                <div className="ReusableButton">
+                    <Button
+                        variant="primary"
+                        size="lg"
+                        block
+                        onClick={this._handleClassSubmit}>
+                        Manage quizzes
+                    </Button>
+                </div>
+            </div>
+        )
+    }
+}
